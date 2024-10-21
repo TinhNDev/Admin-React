9c9be7fadc8b4ca75d0584b035507114e8d4adee
Nguyễn Thanh Tịnh <142908230+MrToInh@users.noreply.github.com>
2024-10-22 02:03:34 +0700

    second

---
+++ src/components/Cart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {MdOutlineCancel} from 'react-icons/md'
+import {AiOutlinePlus, AiOutlineMinus} from 'react-icons/ai'
+
+import { useStatContext } from "../context/ContextProvider";
+import Button from "./Button";
+
+const Cart = () =>{
+    const {currentColor} = useStatContext();
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
