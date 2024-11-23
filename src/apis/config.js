c147cd26ddf21744b1b9c11a4976de999e9c02ba
Nguyễn Thanh Tịnh <nttinh102003@gmail.com>
2024-11-23 13:51:09 +0700

    123

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    API_URL: 'http://localhost:8080/v1/api', // Đổi URL này thành URL của bạn
+    API_URL: 'http://ose.id.vn/v1/api', // Đổi URL này thành URL của bạn
     API_KEY: '123' // Đổi thành khóa API của bạn
   };
   
