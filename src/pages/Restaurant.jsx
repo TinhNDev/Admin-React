32a2c2b3a06ae66df56e5944fbd3bb12f4413372
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2025-02-18 12:43:40 +0700

    add restaurant page

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Restaurant = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            restaurant
+        </div>
+    );
 };
 
 export default Restaurant;
