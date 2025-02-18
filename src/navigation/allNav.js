6e0eea71c1d51abf83223a56ee8d264ac4bdc701
Đỗ Huỳnh Bảo Đăng <124235658+BaoDang0209@users.noreply.github.com>
2025-02-18 13:03:48 +0700

    update routes

--- src/navigation/allNav.js
+++ src/navigation/allNav.js
@@ -1,5 +1,9 @@
 import { AiOutlineDashboard } from "react-icons/ai";
 import { PiUsersThreeFill } from "react-icons/pi";
+import { IoRestaurant } from "react-icons/io5";
+import { MdLocalShipping } from "react-icons/md";
+import { FaBowlFood } from "react-icons/fa6";
+import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
 
 export const allNav = [
     {
@@ -14,8 +18,37 @@ export const allNav = [
         title : 'Customers',
         icon : <PiUsersThreeFill />,
         role : 'admin',
+        path: '/admin/customer'
+    },
+    {
+        id : 3,
+        title : 'Restaurant',
+        icon : <IoRestaurant />,
+        role : 'admin',
         path: '/admin/restaurant'
     },
+    {
+        id : 4,
+        title : 'Shipper',
+        icon : <MdLocalShipping />,
+        role : 'admin',
+        path: '/admin/shipper'
+    },
+    {
+        id : 5,
+        title : 'Product',
+        icon : <FaBowlFood />,
+        role : 'admin',
+        path: '/admin/product'
+    },
+    {
+        id : 6,
+        title : 'LiveChat',
+        icon : <IoChatbubbleEllipsesSharp />,
+        role : 'admin',
+        path: '/admin/live-chat'
+    },
+
 
 
 ]
--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,6 +1,10 @@
 import { lazy } from "react";         
 const Dashboard = lazy(()=> import('../../pages/Dashboard'))
 const Restaurant = lazy(()=> import('../../pages/Restaurant'))
+const Customer = lazy(()=> import('../../pages/Customer'))
+const Shipper = lazy(()=> import('../../pages/Shipper'))
+const Product = lazy(()=> import('../../pages/Product'))
+const LiveChat = lazy(()=> import('../../pages/LiveChat'))
 
 
 export const adminRoutes = [
@@ -12,6 +16,26 @@ export const adminRoutes = [
     {
         path: 'admin/restaurant',
         element : <Restaurant/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/customer',
+        element : <Customer/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/shipper',
+        element : <Shipper/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/product',
+        element : <Product/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/live-chat',
+        element : <LiveChat/>,
         role : 'admin'
     },
     
