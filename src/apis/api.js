468497282cd22469d0f67324cd3e673b07d86796
tinhnt <nttinh102003@gmail.com>
2025-05-22 22:08:16 +0700

    --no-edit

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://sbr09801-8080.asse.devtunnels.ms/v1/api",
-  baseURL: "http://localhost:8080/v1/api/",
+  baseURL: "http://ose.id.vn/v1/api",
   headers: {
     "x-api-key": "123",
     "Content-Type": "application/json",
